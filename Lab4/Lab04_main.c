6591ddf4849ed76c3fd25601aee4e12cc9c6f7aa
PlanetCentral <Rahil7557@gmail.com>
2024-05-01 16:15:17 -0700

    Lab4_main.c error handling

--- Lab4/Lab04_main.c
+++ Lab4/Lab04_main.c
@@ -1,39 +1,79 @@
 // **** Include libraries here ****
-// Standard libraries
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
-//Support Library
+// Support Library
 #include "BOARD.h"
 
 // User libraries
 #include "rpn.h"
 
 // **** Set macros and preprocessor directives ****
-// The lab calls for 60 characters of user input
 #define MAX_INPUT_LENGTH 60
+#define ERROR_EXCEED_INPUT_LENGTH 7
 
 int main() {
     BOARD_Init();
-
-    //What follows is starter code.  You will need to modify it!
-
     char rpn_sentence[MAX_INPUT_LENGTH + 2];
     double result;
+    int returnVal;
+    char printString[MAX_INPUT_LENGTH + 50];  // Increased buffer size for the error messages.
 
-    printf("Welcome to Rahil's RPN calculator. Compiled on %s %s", __DATE__, __TIME__". ");
+    printf("Welcome to Rahil's RPN calculator. Compiled on %s %s.\n", __DATE__, __TIME__);
+
     while (1) {
-
         printf("Enter floats and + - / * in RPN format, for example, '1 2 +':\n");
 
-        fgets(rpn_sentence, sizeof (rpn_sentence), stdin);
+        if (fgets(rpn_sentence, sizeof(rpn_sentence), stdin) == NULL) {
+            continue;  // Handles unexpected errors in the input.
+        }
 
-        RPN_Evaluate(rpn_sentence, &result);
+        // Finds and replace the newline chars with a null terminator.
+        int iter = 0;
+        while (rpn_sentence[iter] != '\n' && rpn_sentence[iter] != '\0') {
+            iter++;
+        }
+        rpn_sentence[iter] = '\0';
 
-        printf("result = %f\n", result);
+        // Checks if the input length exceeds the max length.
+        if (iter > MAX_INPUT_LENGTH) {
+            returnVal = ERROR_EXCEED_INPUT_LENGTH;
+        } else {
+            ProcessBackspaces(rpn_sentence);
+            returnVal = RPN_Evaluate(rpn_sentence, &result);
+        }
 
+        // Various error cases (5/5)
+        switch (returnVal) {
+            case 1:
+                sprintf(printString, "ERROR: There is no more room on the stack.\n");
+                break;
+            case 2:
+                sprintf(printString, "ERROR: There aren't enough operands before the operator.\n");
+                break;
+            case 3:
+                sprintf(printString, "ERROR: Invalid characters used in the RPN String.\n");
+                break;
+            case 4:
+                sprintf(printString, "ERROR: Invalid RPN calculation; divided by 0.\n");
+                break;
+            case 5:
+                sprintf(printString, "ERROR: Invalid RPN calculation; no items on the stack.\n");
+                break;
+            case 6:
+                sprintf(printString, "ERROR: Invalid RPN calculation; too many items on the stack.\n");
+                break;
+            case ERROR_EXCEED_INPUT_LENGTH:
+                sprintf(printString, "ERROR: Input string length exceeds 60 characters. At maximum please enter 60 characters.\n");
+                break;
+            default:
+                sprintf(printString, "Result = %lf\n", result);
+                break;
+        }
+
+        printf("%s", printString);
     }
 
-    while (1);
+    return 0;
 }
